cc438065e9d4f40df8db28be3de461f1aab50be5
jealfonso <jorge@qbex.com>
2019-08-15 18:42:11 -0400

    mavendemos ok

---
+++ mavendemo/src/main/java/com/learnmaven/mavendemo/Calculator.java
@@ -0,0 +1,28 @@
+package com.mavendemo;
+
+public class Calculator {
+
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public double divide(int num1, int num2) {
+		if(num2 == 0) {
+			return 0.0;
+		}else {
+			return num1 / num2;
+		}	
+	}
+
+
+
+
+}
---
+++ mavendemo/src/test/java/com/mavendemo/AppTest.java
@@ -0,0 +1,44 @@
+package com.mavendemo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+	
+	public AppTest( String testName ) {
+		
+		super (testName);
+	}
+	
+	public static Test suite() {
+		
+		return new TestSuite( AppTest.class );
+		
+	}
+	
+	public void testAdd() {
+		
+		assertEquals(10, new Calculator().add(5, 5) );
+		
+	}
+
+	public void testSubtract() {
+		
+		assertEquals(10, new Calculator().subtract(20, 10) );
+		
+	}
+	
+	public void testMultiply() {
+		
+		assertEquals(10, new Calculator().multiply(5, 2) );
+		
+	}
+
+	public void testDivide() {
+		
+		assertEquals(10.0, new Calculator().divide(100, 10) );
+		
+	}
+	
+}
